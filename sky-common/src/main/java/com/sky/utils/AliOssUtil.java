6f72b34968c16b0d5d1b71cbf78271e77e718e56
hezhiyuan <linkshuyue@gmail.com>
2026-08-29 18:52:11 +0800

    feat: 菜品管理，新建菜品功能开发

--- sky-common/src/main/java/com/sky/utils/AliOssUtil.java
+++ sky-common/src/main/java/com/sky/utils/AliOssUtil.java
@@ -7,6 +7,7 @@ import com.aliyun.oss.OSSException;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+
 import java.io.ByteArrayInputStream;
 
 @Data
@@ -52,7 +53,7 @@ public class AliOssUtil {
             }
         }
 
-        //文件访问路径规则 https://BucketName.Endpoint/ObjectName
+        // 文件访问路径规则 https://BucketName.Endpoint/ObjectName
         StringBuilder stringBuilder = new StringBuilder("https://");
         stringBuilder
                 .append(bucketName)
