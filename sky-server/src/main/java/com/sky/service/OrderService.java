d199c975074dfdd4d8745408e397edb0d36612ed
hezhiyuan <linkshuyue@gmail.com>
2026-09-13 09:49:22 +0800

    feat: 用户端，取消订单功能

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -49,4 +49,11 @@ public interface OrderService {
      * @return
      */
     OrderVO details(Long id);
+
+    /**
+     * 用户取消订单
+     *
+     * @param id
+     */
+    void userCancelById(Long id) throws Exception;
 }
